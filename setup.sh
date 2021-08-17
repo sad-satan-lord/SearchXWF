eb08b00ae54a1486f30bc46d7fdde9568d425cf3
AHAD-1337 <83266343+Ahad1675@users.noreply.github.com>
2021-08-17 16:16:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,17 @@ update_and_install_packages () {
     apt -qq update && apt -qq upgrade -y
     apt install --no-install-recommends -y curl git libffi-dev libjpeg-dev libwebp-dev python3-lxml python3-psycopg2 libpq-dev libcurl4-openssl-dev libxml2-dev libxslt1-dev python3-pip python3-sqlalchemy openssl wget python3 python3-dev libreadline-dev libyaml-dev gcc zlib1g ffmpeg libssl-dev libgconf-2-4 libxi6 unzip libopus0 libopus-dev python3-venv libmagickwand-dev pv tree mediainfo
 }
+
+install_helper_packages () {
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt -fqqy install ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
+    wget https://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod +x chromedriver && mv -f chromedriver /usr/bin/ && rm chromedriver_linux64.zip
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/master.zip && unzip opencv.zip && mv -f opencv-master /usr/bin/ && rm opencv.zip
+    wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel -P ./bot_utils_files/ai_helpers/
+}
   
 _run_all () {
     update_and_install_packages
+    install_helper_packages
     pip3 install –upgrade pip
     pip3 install --no-cache-dir -r requirements.txt
 }
