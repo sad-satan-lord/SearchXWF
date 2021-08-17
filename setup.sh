2097439e07bfc1139ce6f7a09668f7fc96e47223
AHAD-1337 <83266343+Ahad1675@users.noreply.github.com>
2021-08-17 19:27:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ update_and_install_packages () {
 _run_all () {
     update_and_install_packages
     pip3 install –upgrade pip
+    pip3 install -r addons.txt
     pip3 install -r requirements.txt
     pip3 install -r local-requirements.txt
-    pip3 install -r addons.txt
 }
 
 _run_all
