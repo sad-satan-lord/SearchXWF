3a5cea27f41cae1731d51562dc0759e3f88cfb1a
AHAD-1337 <83266343+Ahad1675@users.noreply.github.com>
2021-08-17 16:01:20 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+update_and_install_packages () {
+    apt -qq update && apt -qq upgrade -y
+    apt install --no-install-recommends -y curl git libffi-dev libjpeg-dev libwebp-dev python3-lxml python3-psycopg2 libpq-dev libcurl4-openssl-dev libxml2-dev libxslt1-dev python3-pip python3-sqlalchemy openssl wget python3 python3-dev libreadline-dev libyaml-dev gcc zlib1g ffmpeg libssl-dev libgconf-2-4 libxi6 unzip libopus0 libopus-dev python3-venv libmagickwand-dev pv tree mediainfo
+}
+  
+_run_all () {
+    update_and_install_packages
+    pip3 install –upgrade pip
+    pip3 install --no-cache-dir -r requirements.txt
+}
+
+_run_all
