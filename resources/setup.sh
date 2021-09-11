6b037598e835bb7f8b7156fe849fdbc1d344bcb4
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 13:07:36 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,11 +1,6 @@
 cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
-if [ "$(uname -m)" = "aarch64" ] ; then \
-        export HOST_CPU_ARCH=arm64; \
-    elif [ "$(uname -m)" = "x86_64" ]; then \
-        export HOST_CPU_ARCH=amd64
 pip3 install --no-cache-dir MirrorX
-apt-get -qq install -y tzdata curl aria2 p7zip-full p7zip-rar wget xz-utils libmagic-dev gcc
 wget -q https://github.com/viswanathbalusu/megasdkrest/releases/download/v0.1.1/megasdkrest-${HOST_CPU_ARCH} -O /usr/local/bin/megasdkrest
 chmod a+x /usr/local/bin/megasdkrest
 cp bin/extract /usr/local/bin
