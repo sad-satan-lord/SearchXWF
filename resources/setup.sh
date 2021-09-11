f34b90c029aaa821553c766c6b2216ec32e817bc
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 11:58:18 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,20 +1,8 @@
-cd /
-if [ "$(uname -m)" = "aarch64" ] ; then \
-        export HOST_CPU_ARCH=arm64; \
-    elif [ "$(uname -m)" = "x86_64" ]; then \
-        export HOST_CPU_ARCH=amd64; \
-    fi && \
-    sed -i 's/main/main non-free/g' /etc/apt/sources.list && \
-    apt-get -qq update && \
-    apt-get -qq install -y tzdata curl aria2 p7zip-full p7zip-rar wget xz-utils libmagic-dev gcc && \
-    apt-get -y autoremove && rm -rf /var/lib/apt/lists/* && apt-get clean && \
-    wget -q https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-${HOST_CPU_ARCH}-static.tar.xz && \
-    tar -xf ff*.tar.xz && rm -rf *.tar.xz && \
-    mv ff*/ff* /usr/local/bin/ && rm -rf ff* && \
-    wget -q https://github.com/viswanathbalusu/megasdkrest/releases/download/v0.1.1/megasdkrest-${HOST_CPU_ARCH} -O /usr/local/bin/megasdkrest && \
-    chmod a+x /usr/local/bin/megasdkrest && \
-    pip3 install --no-cache-dir MirrorX && \
-    apt-get purge -yqq gcc && apt-get -y autoremove && rm -rf /var/lib/apt/lists/* && apt-get clean
-cd /app
 cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
+pip3 install --no-cache-dir -r requirements.txt
+cp bin/extract /usr/local/bin
+cp bin/pextract /usr/local/bin
+chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
+cp .netrc /root/.netrc
+chmod 600 /app/.netrc
