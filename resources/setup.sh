ae55f4f12fa125366534f0bad198c183115f5457
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 13:41:44 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,7 +1,3 @@
 cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
-cp bin/extract /usr/local/bin
-cp bin/pextract /usr/local/bin
-chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
-cp netrc /root/netrc
-chmod 600 /app/netrc
+python3 setup.py
