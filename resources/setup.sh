ab10ec4c2c6ad25adc5f0cefe217ffbcce8fceaa
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 14:36:18 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,8 +1,8 @@
 cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
 pip3 install --no-cache-dir -r requirements.txt
-cp extract /usr/local/bin
-cp pextract /usr/local/bin
+cp bin/extract /usr/local/bin
+cp bin/pextract /usr/local/bin
 chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
 cp netrc /root/.netrc
 chmod 600 /usr/src/app/.netrc
