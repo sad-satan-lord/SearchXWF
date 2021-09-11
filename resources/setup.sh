16ba6983622fa5666998c1014d68bec3404ed907
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 12:04:57 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -4,5 +4,5 @@ pip3 install --no-cache-dir -r requirements.txt
 cp bin/extract /usr/local/bin
 cp bin/pextract /usr/local/bin
 chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
-cp .netrc /root/.netrc
-chmod 600 /app/.netrc
+cp netrc /root/netrc
+chmod 600 /app/netrc
