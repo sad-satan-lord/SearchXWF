9bc89ac41c6857a835d5a7dfc7416cccc0effbd7
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 13:39:15 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,8 +1,5 @@
 cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
-pip3 install --no-cache-dir MirrorX
-wget -q https://github.com/viswanathbalusu/megasdkrest/releases/download/v0.1.1/megasdkrest-${HOST_CPU_ARCH} -O /usr/local/bin/megasdkrest
-chmod a+x /usr/local/bin/megasdkrest
 cp bin/extract /usr/local/bin
 cp bin/pextract /usr/local/bin
 chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
