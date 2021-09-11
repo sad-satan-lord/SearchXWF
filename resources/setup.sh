c3f9dc0ff7597cb03282eb139fff40917b3a36de
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-11 12:58:09 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -2,7 +2,7 @@ cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
 pip3 install --no-cache-dir -r requirements.txt
 apt-get -qq install wget -y
-wget -q https://github.com/viswanathbalusu/megasdkrest/releases/download/v0.1.1/megasdkrest-x86_64 -O /usr/local/bin/megasdkrest
+wget -q https://github.com/viswanathbalusu/megasdkrest/releases/download/v0.1.1/megasdkrest-aarch64 -O /usr/local/bin/megasdkrest
 chmod a+x /usr/local/bin/megasdkrest
 cp bin/extract /usr/local/bin
 cp bin/pextract /usr/local/bin
