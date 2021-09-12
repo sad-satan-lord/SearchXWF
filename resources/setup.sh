17a7a4b7525d501cb54ede055df56ff0e30fba67
» X E R O « <83266343+Xero1675@users.noreply.github.com>
2021-09-12 19:17:07 +0530

    Update setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,6 +1,5 @@
 cp resources/config.env /app
 cp resources/token.pickle /app &> /dev/null
-pip3 install --no-cache-dir -r requirements.txt
 cp bin/extract /usr/local/bin
 cp bin/pextract /usr/local/bin
 chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
